1e0a36d6dd69f1faa79907723c45956811de74a8
Pablo <pablo.fontanillaarranz@ceu.es>
2024-12-03 23:28:51 +0100

    Day 3 part 2 solution

--- 2024/03/code.go
+++ 2024/03/code.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jpillora/puzzler/harness/aoc"
 	"regexp"
 	"strconv"
@@ -8,6 +9,22 @@ import (
 
 func main() {
 	aoc.Harness(run)
+}
+
+func getSubResult(input string) int {
+	subTotalSum := 0
+	// Compile the regular expression
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+
+	// Find all matches
+	matches := re.FindAllStringSubmatch(input, -1)
+	// Iterate through the matches
+	for _, match := range matches {
+		num1, _ := strconv.Atoi(match[1])
+		num2, _ := strconv.Atoi(match[2])
+		subTotalSum += num1 * num2
+	}
+	return subTotalSum
 }
 
 // on code change, run will be executed 4 times:
@@ -18,21 +35,21 @@ func main() {
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
 
-	// Compile the regular expression
-	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-
-	// Find all matches
-	matches := re.FindAllStringSubmatch(input, -1)
-
-	totalSum := 0
-	// Iterate through the matches
-	for _, match := range matches {
-		num1, _ := strconv.Atoi(match[1])
-		num2, _ := strconv.Atoi(match[2])
-		totalSum += num1 * num2
-	}
+	totalSum := getSubResult(input)
+	var part2TotalSum = 0
 	if part2 {
-		return "42"
+		//We get all the validated tuples (start with do or start-of-line and finish with don't())
+		re2 := regexp.MustCompile(`((do\(\)|^)(.+?)(don't\(\)))`)
+
+		negativeMatches := re2.FindAllStringSubmatch(input, -1)
+		fmt.Printf("Found %v matches\n", len(negativeMatches))
+
+		//Calculate and accumulate the subtotal from each match's tuples
+		for _, match := range negativeMatches {
+			part2TotalSum += getSubResult(match[3])
+		}
+
+		return part2TotalSum
 	}
 	// solve part 1 here
 	return totalSum
